96fa3a825804607324b7b28c03124db84b3e2279
CoookieSoup <SuperHackingNinja@gmail.com>
2023-03-21 21:45:39 +0200

    Update
    Worked on wall jump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,17 +9,107 @@ public class PlayerScript : MonoBehaviour
     public float jumpStrength;
     public float speedStrength;
     private float horizontal;
-    public SpriteRenderer sprite;
-
+    
     public Transform groundCheck;
     public LayerMask groundLayer;
     public bool isGrounded;
 
     public bool isOnPlatform;
     public Rigidbody2D platformRb;
-    public Animator animator;
 
+    public SpriteRenderer sprite;
+    public Animator animator;
     public bool flipX;
+
+    private bool isWallSliding;
+    public float wallSlideSpeed = 7f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.1f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(10f, 20f);
+
+
+
+
+    // Wall slide logic start
+
+    private bool IsTouchingWall()
+    {
+        return Physics2D.OverlapBox(wallCheck.position, new Vector2(4.1f, 4f), 0, groundLayer);
+    }
+
+    private void WallSlide ()
+    {
+        if (IsTouchingWall() && isGrounded == false && horizontal != 0f)
+        {
+            isWallSliding = true;
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Mathf.Clamp(myRigidbody.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    // Wall slide logic end
+
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            if (flipX == true)
+            {
+                wallJumpingDirection = -1f;
+            }
+            if (flipX == false)
+            {
+                wallJumpingDirection = 1f;
+            }
+            wallJumpingCounter = wallJumpingTime;
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            myRigidbody.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+            if(flipX == false && wallJumpingDirection == -1)
+            {
+                flipX = true;
+            }
+            if (flipX == true && wallJumpingDirection == 1)
+            {
+                flipX = false;
+            }
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+
+    }
+     private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+
+
+
+
+
+    // Wall jump logic start
+
+    // Wall jump logic end
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +122,6 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //grounded check, tie skaiciai su f yra hardcoded, tu scenoje ant isGrounded object uzdek CapsuleColider, settink i horizontal ir kokie skaiciukai tokius duek kad jump hitbox
         isGrounded = Physics2D.OverlapBox(groundCheck.position, new Vector2(2.2f, 0.2f), 0, groundLayer);
         horizontal = Input.GetAxisRaw("Horizontal");
         myRigidbody.velocity = new Vector2(horizontal * speedStrength, myRigidbody.velocity.y);
@@ -78,6 +167,10 @@ public class PlayerScript : MonoBehaviour
         {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x + platformRb.velocity.x, myRigidbody.velocity.y);
         }
+
+
+        WallSlide();
+        WallJump();
 
     }
     
