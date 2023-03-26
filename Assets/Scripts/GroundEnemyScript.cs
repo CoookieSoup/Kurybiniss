1dcc4dcaa1ec5bc0b96b0cf0225ecc80ffba469f
CoookieSoup <SuperHackingNinja@gmail.com>
2023-03-26 22:28:09 +0300

    Update
    Fixed raycasting shenanigans, ground enemy line of sight improvements

--- Assets/Scripts/GroundEnemyScript.cs
+++ Assets/Scripts/GroundEnemyScript.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GroundEnemyScript : MonoBehaviour
 {
-    RaycastHit2D hit;
+    //RaycastHit2D hit;
+    RaycastHit2D hitUpper;
     private Transform playerPos;
     private Rigidbody2D GroundEnemyRb;
+    private Transform GroundEnemyLOSCheck;
     [SerializeField] private float GroundEnemySpeedStrength = 10f;
     [SerializeField] private float EnemyDetectRange = 10f;
 
@@ -16,6 +18,7 @@ public class GroundEnemyScript : MonoBehaviour
     {
         playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         GroundEnemyRb = GetComponent<Rigidbody2D>();
+        GroundEnemyLOSCheck = GetComponent<Transform>();
     }
 
     void OnCollisionEnter2D(Collision2D collider)
@@ -29,8 +32,9 @@ public class GroundEnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hit = Physics2D.Raycast(transform.position, playerPos.position - transform.position);
-        if (hit.collider.gameObject.CompareTag("Player") && Mathf.Abs(playerPos.position.x - transform.position.x) < EnemyDetectRange) //31
+        hitUpper = Physics2D.Raycast(new Vector3 (GroundEnemyLOSCheck.position.x, GroundEnemyLOSCheck.position.y + 2f), playerPos.position - GroundEnemyLOSCheck.position);
+        Debug.DrawRay(new Vector3(GroundEnemyLOSCheck.position.x, GroundEnemyLOSCheck.position.y + 2f), playerPos.position - GroundEnemyLOSCheck.position, Color.green);
+        if (hitUpper.collider.gameObject.CompareTag("Player") && Mathf.Abs(playerPos.position.x - transform.position.x) < EnemyDetectRange) //31
         {
             if (playerPos.position.x - transform.position.x < 0f)
             {
@@ -41,7 +45,12 @@ public class GroundEnemyScript : MonoBehaviour
                 GroundEnemyRb.velocity = new Vector2(GroundEnemySpeedStrength, GroundEnemyRb.velocity.y);
             }
         }
-        if (hit.collider.gameObject.CompareTag("Player") == false || Mathf.Abs(playerPos.position.x - transform.position.x) >= EnemyDetectRange)
+        
+        if (hitUpper.collider.gameObject.CompareTag("Player") == false)
+        {
+            GroundEnemyRb.velocity = new Vector2(0f, 0f);
+        }
+        if (Mathf.Abs(playerPos.position.x - transform.position.x) >= EnemyDetectRange)
         {
             GroundEnemyRb.velocity = new Vector2(0f, 0f);
         }
